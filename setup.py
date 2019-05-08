89608155dea7de1b2c3dd3404c86c436738a7049
sethah <shendrickson@cloudera.com>
2019-05-08 14:37:43 -0700

    remove debug file; add install requires

--- setup.py
+++ setup.py
@@ -7,4 +7,20 @@ setup(
     description='',
     author='sethah',
     license='MIT',
+    python_requires='>=3.6.1',
+    install_requires=[
+        'torch>=0.4.1',
+        'torchvision',
+        'allennlp',
+        'numpy',
+        'overrides',
+        'ipython',
+        'albumentations',
+        'Pillow',
+        'overrides',
+        'numpy',
+      ],
+    tests_require=[
+        'pytest'
+    ]
 )
