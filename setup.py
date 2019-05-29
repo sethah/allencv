07f5e2c0dae744f1f734a9661fc15d0fd9e5e17c
sethah <shendrickson@cloudera.com>
2019-05-29 12:18:19 -0700

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='allencv',
     packages=find_packages(),
-    version='0.1.1',
+    version='0.1.2-unreleased',
     description='',
     author='sethah',
     license='MIT',
