3a109d0cd502ec832d2812eba850a5df1b178985
sethah <shendrickson@cloudera.com>
2019-05-30 16:34:41 -0700

    Release: 0.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='allencv',
     packages=find_packages(),
-    version='0.1.3-unreleased',
+    version='0.1.3',
     description='',
     author='sethah',
     license='MIT',
