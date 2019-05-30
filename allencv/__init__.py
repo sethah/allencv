f7010b9f1e37b913189b4a978dddd93ae63fd0a9
sethah <shendrickson@cloudera.com>
2019-05-30 09:19:47 -0700

    remove cuda version hack

--- allencv/__init__.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-# apex requirement causes problems when cuda is unavailable
-# TODO: this is not cool in general to do.
-if not torch.cuda.is_available():
-    torch.version.cuda = "9.0"
